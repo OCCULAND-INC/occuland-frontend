b14b0391b83ea3f5262214c18159fd48f81e6773
Dream High <smartcoder0415@gmail.com>
2022-01-05 20:34:26 -0500

    refactored LandDetailModal

--- src/components/global/Modal/Modal.tsx
+++ src/components/global/Modal/Modal.tsx
@@ -34,7 +34,7 @@ function Modal({ isOpen, children, onClose }: ModalProps) {
           &#8203;
         </span>
         {/* modal panel */}
-        <div className="flex text-base text-left transform transition w-full md:inline-block md:max-w-2xl md:px-4 md:my-8 md:align-middle lg:max-w-4xl">
+        <div className="flex text-base text-left transform transition w-full md:inline-block md:max-w-2xl md:px-4 md:my-8 md:align-middle lg:max-w-2xl">
           <div className="w-full relative flex items-center bg-white px-4 pt-14 pb-8 overflow-hidden shadow-2xl sm:px-6 sm:pt-8 md:p-6 lg:p-8">
             <button
               onClick={onClose}
@@ -59,27 +59,7 @@ function Modal({ isOpen, children, onClose }: ModalProps) {
               </svg>
             </button>
 
-            <div className="w-full grid grid-cols-1 gap-y-8 gap-x-6 items-start sm:grid-cols-12 lg:gap-x-8">
-              <div className="aspect-w-2 aspect-h-3 rounded-lg bg-gray-100 overflow-hidden sm:col-span-4 lg:col-span-5">
-                <img
-                  src="https://tailwindui.com/img/ecommerce-images/product-quick-preview-02-detail.jpg"
-                  alt="Two each of gray, white, and black shirts arranged on table."
-                  className="object-center object-cover"
-                />
-              </div>
-              <div className="sm:col-span-8 lg:col-span-7">
-                <h2 className="text-2xl font-extrabold text-gray-900 sm:pr-12">
-                  Basic Tee 6-Pack
-                </h2>
-                <section aria-labelledby="information-heading" className="mt-2">
-                  <h3 id="information-heading" className="sr-only">
-                    Product information
-                  </h3>
-                  <p className="text-2xl text-gray-900">$192</p>
-                  {children}
-                </section>
-              </div>
-            </div>
+            {children}
           </div>
         </div>
       </div>
--- src/components/pages/Land/LandDetailModal/LandDetailModal.tsx
+++ src/components/pages/Land/LandDetailModal/LandDetailModal.tsx
@@ -5,7 +5,51 @@ type Props = Omit<ModalProps, 'children'>;
 function LandDetailModal({ isOpen, onClose }: Props) {
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
-      children
+      <div className="w-full grid grid-cols-1 gap-y-8 gap-x-6 items-start sm:grid-cols-12 lg:gap-x-8">
+        <div className="sm:col-span-8 lg:col-span-7">
+          <h2 className="text-2xl font-extrabold text-gray-900 sm:pr-12">
+            Contract Detail
+          </h2>
+          <section aria-labelledby="information-heading" className="mt-2">
+            <h3 id="information-heading" className="sr-only">
+              Product information
+            </h3>
+            <table>
+              <tr>
+                <th>Landing Renting</th>
+                <td className="pl-5">19, 6</td>
+              </tr>
+              <tr>
+                <th>Time in </th>
+                <td className="pl-5">December 20, 2021</td>
+              </tr>
+              <tr>
+                <th>Time out</th>
+                <td className="pl-5">Decemeber 22, 2021</td>
+              </tr>
+              <tr>
+                <th>Number of days</th>
+                <td className="pl-5">4</td>
+              </tr>
+            </table>
+
+            <table className="mt-5">
+              <tr>
+                <th>Security Deposit:</th>
+                <td className="pl-5">200 MANA</td>
+              </tr>
+              <tr>
+                <th>Daily Rate:</th>
+                <td className="pl-5">200 MANA</td>
+              </tr>
+              <tr>
+                <th>Transfer Fee:</th>
+                <td className="pl-5">$30.00</td>
+              </tr>
+            </table>
+          </section>
+        </div>
+      </div>
     </Modal>
   );
 }
