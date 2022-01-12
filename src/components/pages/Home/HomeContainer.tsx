85ee43e69b65a464486fa359e2751326ad7ec784
Christian Martinez <christianbm1@gmail.com>
2022-01-12 00:06:01 -0500

    changes to home. select box for platform, aligneg content center, similar widths
    formatted select box in land market filters
    
    formatted select boxes/buttons home/landmarket

--- src/components/pages/Home/HomeContainer.tsx
+++ src/components/pages/Home/HomeContainer.tsx
@@ -1,14 +1,26 @@
 import Button from '~/components/global/Button/Button';
+import Select from '~/components/global/Select/Select';
 import Input from '~/components/global/Input/Input';
+import { useState } from 'react';
+
 
 function HomeContainer() {
+  const [selection, setSelection] = useState<string>('0');
+
   return (
     <div className="container mx-auto h-full flex flex-col justify-center items-center">
       <h1 className="text-3xl mb-5">Explore the Metaverse</h1>
-      <form className="flex">
-        <Input type="password" className="h-10 mr-5" placeholder="Platform" />
-        <Button className="mr-5">Land market</Button>
-        <Button>Experience</Button>
+      <form className="flex home-search-form">
+        <Select 
+        defaultValue={selection}
+        onChange={(e) => setSelection(e.value != '0' ? e.value : selection)}
+        options={[
+          {text: 'Choose Platform:', value:'0'},
+          {text: 'Decentraland', value:'1'},
+          {text: 'The Sandbox', value:'2'}
+        ]}></Select>
+        <Button className="mr-5">Search Land</Button>
+        <Button>Search Experiences</Button>
       </form>
     </div>
   );
